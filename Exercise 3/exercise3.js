0675c1e25d1a6af0e9488764e3d341c3b8496292
sum-rg-blip <sumayamoha567@gmail.com>
2026-01-10 14:52:32 +0300

    exercise 3

---
+++ Exercise 3/exercise3.js
@@ -0,0 +1,8 @@
+let isSunny = true;
+let isWeekend = false;
+console.log("Logical AND (&&) operator results:");
+console.log(isSunny && isWeekend); 
+console.log("Logical OR (||) operator results:");
+console.log(isSunny || isWeekend); 
+console.log("Logical NOT (!) operator results:");
+console.log(!isSunny);             

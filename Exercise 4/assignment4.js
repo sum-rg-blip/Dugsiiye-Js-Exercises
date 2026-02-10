f48ffb82266eb36cce15f9f787fee5049bb71068
sum-rg-blip <sumayamoha567@gmail.com>
2026-02-10 11:25:51 +0300

    exercise 4

---
+++ Exercise 4/assignment4.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+console.log (add(5, 10));
+console.log (add(20, 80));
+
+const Add=function(a, b) {
+    return a + b;
+}
+console.log (Add(15, 25));
+console.log (Add(30, 70));

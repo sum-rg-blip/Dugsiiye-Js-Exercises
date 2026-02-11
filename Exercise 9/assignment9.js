9eecbe7387211da1aed40e7d5cc0e03c550dc258
sum-rg-blip <sumayamoha567@gmail.com>
2026-02-11 12:46:59 +0300

    exercise 9

---
+++ Exercise 9/assignment9.js
@@ -0,0 +1,13 @@
+let car = {
+  make: "Honda",
+  model: "Civic",
+  year: 2023,
+  start: function() {
+        console.log("The car has started");
+    }
+};
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+car.start();

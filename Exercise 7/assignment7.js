f7d5fa45f97422fdc0312f70b911cd47a06cf79c
sum-rg-blip <sumayamoha567@gmail.com>
2026-02-11 11:29:36 +0300

    exercise 7

---
+++ Exercise 7/assignment7.js
@@ -0,0 +1,9 @@
+let colors = ["purple", "yellow", "black"];
+console.log(colors[0]); 
+console.log(colors[1]); 
+console.log(colors[2]); 
+colors.push("blue");
+console.log(colors); 
+
+colors.shift();
+console.log(colors);

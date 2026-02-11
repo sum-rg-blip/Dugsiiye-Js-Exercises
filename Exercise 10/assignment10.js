f0d7dad35299ad198b79234456ac360990bad432
sum-rg-blip <sumayamoha567@gmail.com>
2026-02-11 15:09:05 +0300

    exercise 10

---
+++ Exercise 10/assignment10.js
@@ -0,0 +1,21 @@
+let library = [
+  {
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    year: 1988
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    year: 2018
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    year: 2008
+  }
+];
+
+console.log(library[0].title);
+
+console.log(library[1].author);
